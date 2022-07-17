a4035452f67cf6c650385b258d38edb617024a6a
daigoro22 <abe.daigoro.sb@tut.jp>
2022-07-17 09:48:06 +0900

    Add initial model and view function
    初期化したモデルと view 関数を追加

--- picshare/src/Picshare.elm
+++ picshare/src/Picshare.elm
@@ -4,6 +4,13 @@ import Html exposing (Html, div, h1, h2, img, text)
 import Html.Attributes exposing (class, src)
 
 
+initialModel : { url : String, caption : String }
+initialModel =
+    { url = baseUrl ++ "1.jpg"
+    , caption = "surfing"
+    }
+
+
 main : Html msg
 
 
@@ -12,24 +19,25 @@ main : Html msg
 
 
 main =
+    view initialModel
+
+
+view : { url : String, caption : String } -> Html msg
+view model =
     div []
         [ div [ class "header" ] [ h1 [] [ text "Picshare" ] ]
         , div [ class "content-flow" ]
-            [ viewDetailedPhoto (baseUrl ++ "1.jpg")
-                "surfing"
-            , viewDetailedPhoto
-                (baseUrl ++ "2.jpg")
-                "The fox"
-            , viewDetailedPhoto
-                (baseUrl ++ "3.jpg")
-                "evening"
+            [ viewDetailedPhoto
+                { url = model.url
+                , caption = model.caption
+                }
             ]
         ]
 
 
-viewDetailedPhoto : String -> String -> Html msg
-viewDetailedPhoto url caption =
-    div [ class "detailed-photo" ] [ img [ src url ] [], div [ class "photo-info" ] [ h2 [ class "caption" ] [ text caption ] ] ]
+viewDetailedPhoto : { url : String, caption : String } -> Html msg
+viewDetailedPhoto model =
+    div [ class "detailed-photo" ] [ img [ src model.url ] [], div [ class "photo-info" ] [ h2 [ class "caption" ] [ text model.caption ] ] ]
 
 
 baseUrl : String
